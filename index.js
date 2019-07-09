5993ee384d22549b25dbd8969e45344c214ab344
Kirill Fomichev <fanatid@ya.ru>
2019-07-09 10:33:48 +0300

    move concurrency validation to function

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-module.exports = function (fn, opts) {
-  const concurrency = Object.assign({ concurrency: 1 }, opts).concurrency
-  if (typeof concurrency !== 'number' || isNaN(concurrency) || concurrency <= 0) {
-    throw new TypeError(`Invalid concurrency value: ${concurrency}`)
-  }
+module.exports = function (fn, { concurrency = 1 } = {}) {
+  validateConcurrencyValue(concurrency)
 
   let count = 0
   const queue = []
 
   return async function (...args) {
+    count += 1
+
     try {
-      count += 1
       if (count > concurrency) {
         await new Promise((resolve) => queue.push({ resolve }))
       }
@@ -20,4 +18,9 @@ module.exports = function (fn, opts) {
       if (queue.length > 0) queue.shift().resolve()
     }
   }
+}
+
+function validateConcurrencyValue (value) {
+  const isValid = typeof value === 'number' && value > 0 && (value === Infinity || value % 1 === 0)
+  if (!isValid) throw new TypeError(`Invalid concurrency value: ${value}`)
 }
